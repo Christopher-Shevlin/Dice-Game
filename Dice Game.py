24d9177288d3e3bf5a50a7456adc0de6cee6d74f
Christopher Shevlin <47715649+Christopher-Shevlin@users.noreply.github.com>
2021-04-04 10:50:38 +0100

    Commit
    Moved while True statement to correct position and fixed indentation

--- Dice Game.py
+++ Dice Game.py
@@ -1,14 +1,15 @@
 from random import randint
 
-number = randint(1,6)
-score = 0
 
 while True:
+    number = randint(1,6)
+    score = 0
+
+
     print("Roll = " + str(number))
     print("Your score so far is: ", score)
     print("What's it to be? Higher or Lower?")
     user = input("Higher (H) or Lower (L) > ")
-
 
 
 
