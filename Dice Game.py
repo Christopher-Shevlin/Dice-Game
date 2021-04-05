340bb78371fba06404d403464f3aa8e43dff9cfb
Christopher Shevlin <47715649+Christopher-Shevlin@users.noreply.github.com>
2021-04-05 12:16:04 +0100

    Commit
    Add additional user input and amend while loop

--- Dice Game.py
+++ Dice Game.py
@@ -1,16 +1,23 @@
 from random import randint
 
+start =  False
+name = input("What is your name? > ")
 
-while True:
-    number = randint(1,6)
-    score = 0
-
-
-    print("Roll = " + str(number))
-    print("Your score so far is: ", score)
-    print("What's it to be? Higher or Lower?")
-    user = input("Higher (H) or Lower (L) > ")
-    print("**********************************************")
+while start == False:
+             print(f"Hello", name, """This is the Higher / Lower game, simply decide if the next roll will be higher or lower
+than the previous. Get it right you get 1 point. Get wrong you lose 2 points! Get to 10 points
+to be the winner!""")
+             number = randint(1,6)
+             score = 0
 
 
-
+             print("Roll = " + str(number))
+             print("Your score so far is: ", score)
+             print("What's it to be? Higher or Lower?")
+             user = input("Higher (H) or Lower (L) > ")
+             print("**********************************************")
+             if user == 'H':
+                 start = True
+                 if number <7:
+                    print("Test")
+                    
